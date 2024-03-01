f1dcd621cdf3be5a0e961b87ebf7efe581cbf43f
Abdullah <abdullahamer323@gmail.com>
2024-03-01 21:12:07 +0200

    using StringBuilder for output + Saving the output in file

--- Program.cs
+++ Program.cs
@@ -1,4 +1,5 @@
 ﻿using System.Drawing;
+using System.Text;
 namespace Image2AsciiArt
 {
     public class Program
@@ -12,10 +13,9 @@ namespace Image2AsciiArt
             const double Asciiwidth = 680;
             int ScallingFactor = (int)Math.Ceiling(Asciiwidth / ImageWidth);
             int AsciiHeight = Image.Height * ScallingFactor;
-            //Bitmap Result = new Bitmap((int)Asciiwidth,AsciiHeight);
-            char[,] result = new char[AsciiHeight, (int)Asciiwidth];
-            //int[] scales = {0, 1, 2, 3, 4, 5, 6, 7, 8,9,10,11,12,13,14,15 };
+            
             char[] _AsciiChars = { '#', '#', '@', '%', '=', '+', '*', ':', '-', '.', ' ' };
+            StringBuilder stringBuilder = new StringBuilder();
             for(int i =  0; i < AsciiHeight; i++) 
             {
                 for(int j = 0; j<Asciiwidth; j++) 
@@ -24,18 +24,18 @@ namespace Image2AsciiArt
                     int c = (temp.R + temp.B + temp.G) / 3;
                     Color gray = Color.FromArgb(c, c, c);
                     int indx = (gray.R*10) / 255;
-                    result[i, j] = _AsciiChars[indx];
+                    
+                    stringBuilder.Append(_AsciiChars[indx]);
                 }
+                stringBuilder.Append('\n');
             }
-            for(int i = 0; i<AsciiHeight; i++)
-            {
-                for(int j = 0; j<Asciiwidth; j++)
-                {
-                    Console.Write(result[i,j]);
-                }
-                Console.WriteLine();
-            }
+            StreamWriter sw = new StreamWriter(@"D:\sample.txt");
+            sw.Write(stringBuilder);
+            sw.Flush();
+            sw.Close();
             
+            Console.WriteLine(stringBuilder);
+
         }
     }
 }
